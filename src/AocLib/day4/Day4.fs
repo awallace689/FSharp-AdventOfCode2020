46321d0f0e6d195a467574c8ca3aaac62509a6cf
Adam Wallace <awallace689@gmail.com>
2021-08-05 23:22:26 -0500

    start day4

---
+++ src/AocLib/day4/Day4.fs
@@ -0,0 +1,5 @@
+﻿namespace AocLib
+
+module Day4 =
+    let x a = Array.map <| (+) 3 <| a
+
---
+++ src/AocLib/day4/Parser.fs
@@ -0,0 +1,12 @@
+﻿namespace AocLib
+
+open FParsec
+
+module Day4 =
+    module internal Parser =
+        let f = pfloat
+        
+        let run = f "2.5"
+        
+    
+
